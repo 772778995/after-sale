f3b4b40edd005a8c68dd9941d856fe72ac197d2a
chendonglan <3140528468@qq.com>
2024-06-05 18:40:02 +0800

    update

--- src/layout/main/index.vue
+++ src/layout/main/index.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { CloseBold } from '@element-plus/icons-vue'
+import { Close } from '@element-plus/icons-vue'
 import { useRoute } from 'vue-router'
 const $route = useRoute()
 console.log($route, '66666')
@@ -9,17 +9,17 @@ console.log($route, '66666')
   <div class="breadcrumb">
     <img src="@/assets/images/icon-fold-btns.png" />
     <div class="breadcrumb-box">
-      <div class="breadcrumb-item" v-for="item in 20">
-        <p class="title">订单管理{{ item }}</p>
-        <el-icon color="#606677"><CloseBold /></el-icon>
+      <div class="breadcrumb-item">
+        <p class="title">订单管理</p>
+        <el-icon color="#606677"><Close /></el-icon>
       </div>
     </div>
   </div>
-  <div class="mainContent">
+  <el-scrollbar class="mainContent">
     <router-view v-slot="{ Component }">
       <component :is="Component" />
     </router-view>
-  </div>
+  </el-scrollbar>
 </template>
 
 <style scoped lang="scss">
@@ -41,18 +41,17 @@ console.log($route, '66666')
     flex-wrap: nowrap;
     .breadcrumb-item {
       height: 42px;
-      // background: url('@/assets/images/icon-breadcrumb-bg.png') no-repeat;
-      // background-size: cover;
+      background: url('@/assets/images/icon-breadcrumb-bg.png') no-repeat;
+      background-size: cover;
       display: flex;
       align-items: center;
       white-space: nowrap;
       padding-right: 27px;
       .title {
         text-align: center;
-        font-weight: 700;
         font-size: 16px;
-        // color: #303133;
-        color: #606677;
+        color: #303133;
+        // color: #606677;
         flex: 0.9;
         padding: 0 45px 0 30px;
       }
@@ -60,10 +59,10 @@ console.log($route, '66666')
   }
 }
 .mainContent {
-  height: calc(100% - 64px - 65px);
+  height: calc(100% - 64px - 24px);
   background-color: #f5f5f5;
   padding: 16px 21px 16px 11px;
-  margin: 0 24px 65px 0;
+  margin: 0 24px 24px 0;
   border-radius: 0 10px 10px;
 }
 
