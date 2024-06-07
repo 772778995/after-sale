313893f0dfc639fca089e0b5cb9c564ba4b44741
chendonglan <3140528468@qq.com>
2024-06-07 08:49:10 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,5 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import { onMounted } from 'vue'
-import { reqNavi } from '@/api/user'
-onMounted(async () => {
-  await reqNavi()
-})
 </script>
 
 <template>
